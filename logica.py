1602440828b98eee449f0515cba57fd401abbe8f
cgarrido2001 <c.garridomeneses@uandresbello.edu>
2022-08-25 23:18:49 -0400

    ingresar fichas negras

---
+++ logica.py
@@ -0,0 +1,17 @@
+#negro = -1
+#blanco = 1
+#vacio = 0
+
+class Juegoreversi:
+    def __init__(self, estado=[0]*36, turno=-1):
+        self.tablero=estado
+        self.tablero[14] = -1
+        self.tablero[21] = -1
+        self.tablero[15] = 1
+        self.tablero[20] = 1
+        self.jugador = turno
+
+    def jugar(self, jugada):
+        self.tablero[jugada]=self.jugador   #tanblero[coordenada de jugada] = que jugador es para saber la ficha
+        self.jugador*=-1
+
--- reversi.py
+++ reversi.py
@@ -1,19 +1,22 @@
 from tkinter import *
+import logica
 
 class reversi:
     def __init__(self):
         self.principal = Tk()
         self.principal.title("reversi")
+        self.principal.iconbitmap('./images/tom_cino.ico')
         self.casillas = []
         self.vacio = PhotoImage(file="./images/verde.png")
         self.fichas_blancas = PhotoImage(file="./images/ficha_white.png")
         self.fichas_negras = PhotoImage(file="./images/ficha_nigga.png")
+        self.juego = logica.Juegoreversi()
         for i in range(6):
             fila=[]
             for j in range(6):
                 if ((i==2 and j==2) or (i==3 and j==3)):
                     b1 = Button(self.principal, image=self.fichas_negras, width="100", height="100")
-                    b1.bind("<Button-1>")
+                    b1.bind("<Button-1>", self.click)
                     b1.x=i
                     b1.y=j
                     b1.grid(row=i,column=j)
@@ -21,7 +24,7 @@ class reversi:
 
                 elif ((i==3 and j==2) or (i==2 and j==3)):
                     b1 = Button(self.principal, image=self.fichas_blancas, width="100", height="100")
-                    b1.bind("<Button-1>")
+                    b1.bind("<Button-1>", self.click)
                     b1.x=i
                     b1.y=j
                     b1.grid(row=i,column=j)
@@ -29,13 +32,15 @@ class reversi:
 
                 else:
                     b1=Button(self.principal, image=self.vacio, width="100", height="100")
-                    b1.bind("<Button-1>") #evento del click pendiente
+                    b1.bind("<Button-1>",self.click) #evento del click pendiente
                     b1.x=i
                     b1.y=j
                     b1.grid(row=i,column=j)
                     fila.append(self.vacio)
             self.casillas.append(fila)
-        
-
+    
+    def click(self, evento):
+        if self.juego.tablero[evento.widget.x * 6 + evento.widget.y] == 0:
+            evento.widget["image"] = self.fichas_negras
 juego = reversi()
 mainloop()
